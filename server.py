c64b10218425d6ae093ec4d94d39e4a61ca5417c
Dinesh Padisetti <dinesh.padisetti@gmail.com>
2015-09-06 18:32:52 +0530

    Made changes to server

--- server.py
+++ server.py
@@ -1,11 +1,12 @@
 from flask import *
 import sqlite3
-
+import time
 app = Flask(__name__)
 
 @app.route('/')
 def index():
 	list_of_articles = db_connect()
+	print list_of_articles
 	return render_template('index.html', articles = list_of_articles ,summary = summary)
 
 def summary(article):
@@ -17,9 +18,13 @@ def newpost():
 	if request.method == 'POST':
 		title_title = request.form['title']
 		article_article = request.form['article']
+		time_time = time.ctime()
+		time_list = time_time.split()
+		del time_list[3]
+		time_time = " ".join(time_list)
 		conn = sqlite3.connect('example.db')
 		c = conn.cursor()
-		c.execute('INSERT INTO blog VALUES (NULL, ?, ?)',(title_title,article_article))
+		c.execute('INSERT INTO blog VALUES (NULL, ?, ?, ?)',(title_title,article_article,time_time))
 		conn.commit()
 		conn.close
 		return redirect(url_for('index'))
@@ -29,8 +34,8 @@ def newpost():
 def post(id):
 	conn = sqlite3.connect('example.db')
 	c = conn.cursor()
-	c.execute('SELECT title,article FROM blog where id = %s'%id)
-	title,article = c.fetchone()
+	c.execute('SELECT title,article,time FROM blog where id = %s'%id)
+	title,article,time = c.fetchone()
 	conn.close()
 	return render_template('post.html',title = title,article = article)
 
@@ -38,12 +43,12 @@ def post(id):
 def db_connect():
 	conn = sqlite3.connect('example.db')
 	c = conn.cursor()
-	c.execute('SELECT id,title,article FROM blog order by id DESC')
+	c.execute('SELECT id,title,article,time FROM blog order by id DESC')
 	list_of_articles = c.fetchall()
 	conn.close()
 	return list_of_articles
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
    
 
