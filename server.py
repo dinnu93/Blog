a75b5b4377ff543c627cfe7db0f6bae1404d7a00
Dinesh Padisetti <dinesh.padisetti@gmail.com>
2015-09-16 20:45:31 +0530

    Added a new feature

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 from flask import *
 import sqlite3
-import time
+from datetime import date
+import requests
 app = Flask(__name__)
 
 @app.route('/')
@@ -18,10 +19,8 @@ def newpost():
 	if request.method == 'POST':
 		title_title = request.form['title']
 		article_article = request.form['article']
-		time_time = time.ctime()
-		time_list = time_time.split()
-		del time_list[3]
-		time_time = " ".join(time_list)
+		d_time = date.today()
+		time_time = d_time.strftime("%B %d, %Y")
 		conn = sqlite3.connect('example.db')
 		c = conn.cursor()
 		c.execute('INSERT INTO blog VALUES (NULL, ?, ?, ?)',(title_title,article_article,time_time))
@@ -51,8 +50,42 @@ def projects():
 def contact():
 	return render_template("contact.html")
 
+@app.route('/hyd',methods=['GET','POST'])
+def local():
+	if request.method == 'POST':
+		locality = request.form['locality']
+		lat,lng,polyCoord = code(locality)
+		return render_template("hyd.html",locality = locality,lat = lat,lng = lng, polyCoord = polyCoord)
+	else:
+		return render_template("hyd.html",locality="Hyderabad",lat = 17.385044,lng = 78.486671, polyCoord =[{'lat': 17.2168886, 'lng': 78.1599217}, {'lat': 17.2168886, 'lng': 78.6561694}, {'lat': 17.6078088, 'lng': 78.6561694}, {'lat': 17.6078088, 'lng': 78.1599217}, {'lat': 17.2168886, 'lng': 78.1599217}] )
 
-	
+@app.route('/hyderabad',methods=['GET','POST'])
+def locality():
+	if request.method == 'POST':
+		locality = request.form['locality']
+		lat,lng,polyCoord = code(locality)
+		return render_template("hyderabad.html",locality = locality,lat = lat,lng = lng)
+	else:
+		return render_template("hyderabad.html",locality="Hyderabad",lat = 17.385044,lng = 78.486671)
+
+
+def code(locality):
+	url = "https://maps.googleapis.com/maps/api/geocode/json"
+	key = "AIzaSyC3ugnQvcHCgvybxUXClZIACZMnbDxfmrk"
+	payload = {'key': key,'address': locality+", Hyderabad, Telangana, India"}
+	r = requests.get(url, params=payload)
+	json = r.json()
+	lat =json['results'][0]['geometry']['location']['lat']
+	lng = json['results'][0]['geometry']['location']['lng']
+	lat_sw,lng_sw = json['results'][0]['geometry']['bounds']['southwest']['lat'],json['results'][0]['geometry']['bounds']['southwest']['lng']
+	lat_ne,lng_ne = json['results'][0]['geometry']['bounds']['northeast']['lat'],json['results'][0]['geometry']['bounds']['northeast']['lng']
+	polyCoord = []
+	polyCoord.append({'lat':lat_sw,'lng':lng_sw})
+	polyCoord.append({'lat':lat_sw,'lng':lng_ne})
+	polyCoord.append({'lat':lat_ne,'lng':lng_ne})
+	polyCoord.append({'lat':lat_ne,'lng':lng_sw})
+	polyCoord.append({'lat':lat_sw,'lng':lng_sw})
+	return lat,lng,polyCoord
 
 def db_connect():
 	conn = sqlite3.connect('example.db')
